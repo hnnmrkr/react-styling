41a369e7c893883ff59bfda41bee8bb7e8427bcb
Hanna <hanna-marii.kriisa@voco.ee>
2023-02-02 13:33:01 +0200

    Use styled component for FormControl

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,6 +1,29 @@
 import React, { useState } from "react";
+import styled from "styled-components"
+
 import styles from './CourseInput.module.css'
 import Button from '../../UI/Button';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+  
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => props.invalid ? 'red' : 'black'};
+  }
+  
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+    background: ${props => props.invalid ? 'salmon' : 'transparent'};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+`
 
 const CourseInput = props => {
     const [enteredValue, setEnteredValue] = useState('');
@@ -24,10 +47,10 @@ const CourseInput = props => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className={`styles['form-control']} ${!isValid && styles.invalid}`}>
+            <FormControl invalid={!isValid}>
                 <label>Course Goal</label>
                 <input type="text" onChange={goalInputChangeHandler}/>
-            </div>
+            </FormControl>
             <Button type="submit">Add Goal</Button>
         </form>
     );
